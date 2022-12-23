15c2519da59d721f8133730d6c2219f6efbabbfa
sarathtechversant <sarath.va@techversantinfo.com>
2022-12-23 11:06:58 +0530

    Components and JSX

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello Sarath!</h1>
+// }
+
+//named export
+export const Greet = () => <h1> Hello Sarath!. </h1>
+
+//export default Greet
---
+++ hello-world/src/components/Hello.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hello = () => {
+    // return (
+    //     <div>
+    //         <h1>Hello Sarath!...........</h1>
+    //     </div>
+    // )
+
+    return React.createElement(
+        'div',
+        {id: 'hello',className: 'dummyClass'},
+        React.createElement(
+            'h1',
+            null,
+            'Hello Sarath!.....'
+        ))
+}
+
+export default Hello
